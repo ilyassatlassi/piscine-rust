311a44bc066ac55b7817c5ce772e1d4cad48a10d
ilyassatlassi <atlassiilyass@gmail.com>
2025-07-13 19:41:40 +0100

    temperature

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
-use scalar::*;
+
+use temperature_conv::*;
 
 fn main() {
-    // sum
-    println!("sum: {}", sum(234, 2)); // 'sum: 236'
-    println!("sum: {}", sum(1, 255)); // 'ERROR: attempt to add with overflow'
+    println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
+    println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
+}
 
-    // diff
-    println!("diff: {}", diff(234, 2)); // 'diff: 232'
-    println!("diff: {}", diff(-32768, 32766)); // 'ERROR: attempt to subtract with overflow'
+#[cfg(test)]
+mod tests {
+    use temperature_conv::*;
 
-    // product
-    println!("pro: {}", pro(23, 2)); // 'pro: 46'
-    println!("pro: {}", pro(-128, 2)); // 'ERROR: attempt to multiply with overflow'
+    use std::f64::EPSILON;
 
-    // quotient
-    println!("quo: {}", quo(22.0, 2.0)); // 'quo: 11'
-    println!("quo: {}", quo(-128.23, 2.0)); // 'quo: -64.115'
+    fn eql(a: f64, b: f64) -> bool {
+        (b - a).abs() < EPSILON
+    }
 
-    // remainder
-    println!("rem: {}", rem(22.0, 2.0)); // 'rem: 0'
-    println!("rem: {}", rem(-128.23, 2.0)); // 'rem: -0.22999573'
+    #[test]
+    fn test_f_to_c() {
+        let temp_f = 20.0;
+        println!("{}°F = {}°C", temp_f, fahrenheit_to_celsius(temp_f));
+        assert!(eql(fahrenheit_to_celsius(temp_f), -6.666666666666666));
+        let temp_f = 83.0;
+        println!("{}°F = {}°C", temp_f, fahrenheit_to_celsius(temp_f));
+        assert!(eql(fahrenheit_to_celsius(temp_f), 28.333333333333332));
+    }
+
+    #[test]
+    fn test_c_to_f() {
+        let temp_c = 27.0;
+        println!("{}°C = {}°F", temp_c, fahrenheit_to_celsius(temp_c));
+        assert!(eql(celsius_to_fahrenheit(27.0), 80.6));
+        let temp_c = 0.0;
+        println!("{}°C = {}°F", temp_c, fahrenheit_to_celsius(temp_c));
+        assert!(eql(celsius_to_fahrenheit(temp_c), 32.0))
+    }
 }
