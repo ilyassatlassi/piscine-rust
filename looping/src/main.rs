ca8e073941da118adeadac7102325e1659e228ca
ilyassatlassi <atlassiilyass@gmail.com>
2025-07-14 20:01:31 +0100

    looping

---
+++ looping/src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+    let mut counter = 0;
+    loop {
+        println!(
+            "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
+        );
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        counter += 1;
+        if guess.trim() == "The letter e" {
+            println!("Number of trials: {counter}");
+            break;
+        }
+    }
+}
+
