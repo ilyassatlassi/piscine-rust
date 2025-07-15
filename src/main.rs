d241a6cd19bf2e85d55000799324b9bee44b3a05
ilyassatlassi <atlassiilyass@gmail.com>
2025-07-15 12:11:18 +0100

    add borrow

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,10 @@
-use matrix_transposition::*;
-
+use borrow::*;
 fn main() {
-    let matrix = Matrix((1, 3), (4, 5));
-    println!("Original matrix {:?}", matrix);
-    println!("Transpose matrix {:?}", transpose(matrix));
-}
-#[cfg(test)]
-mod tests {
-    use matrix_transposition::*;
+    let s = "hello";
+    let s1 = "camelCase".to_string();
+    let s2 = "olá!";
 
-    #[test]
-    fn transpose_zero() {
-        let m = Matrix((0, 0), (0, 0));
-        let m = transpose(m);
-        assert_eq!(m, Matrix((0, 0), (0, 0)));
-    }
-
-    #[test]
-    fn transpose_identity() {
-        let m = Matrix((1, 0), (0, 1));
-        let m = transpose(m);
-        assert_eq!(m, Matrix((1, 0), (0, 1)));
-    }
-
-    #[test]
-    fn transpose_other_cases() {
-        let m = Matrix((1, 3), (4, 5));
-        let m = transpose(m);
-        assert_eq!(m, Matrix((1, 4), (3, 5)));
-
-        let m = Matrix((6, 80), (12, 3));
-        let m = transpose(m);
-        assert_eq!(m, Matrix((6, 12), (80, 3)));
-    }
+    println!("\tstr_len(\"{}\") = {}", s, str_len(s));
+    println!("\tstr_len(\"{}\") = {}", s1, str_len(&s1));
+    println!("\tstr_len(\"{}\") = {}", s2, str_len(s2));
 }
