178d33eb7a88f465a9ba719c9613510912f394cb
ilyassatlassi <ilyassatlassi@gmail.com>
2025-07-16 13:49:26 +0100

    add initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,13 +1,8 @@
-fn initials(names: Vec<&str>) -> Vec<String> {
+pub fn initials(names: Vec<&str>) -> Vec<String> {
 names.into_iter().map(|val| {
     match val.split_once(" "){
         Some((first, last)) => format!("{}. {}.", first.chars().next().unwrap(), last.chars().next().unwrap()),
         None => String::new(),
     }
 }).collect()
-}
-
-fn main() {
-    let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"];
-    println!("{:?}", initials(names));
 }
