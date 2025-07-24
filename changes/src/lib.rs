abadb4d9eb007b8bd3d13de4311d861021e19dd1
ilyassatlassi <ilyassatlassi@gmail.com>
2025-07-24 13:34:39 +0100

    add change brightness

--- changes/src/lib.rs
+++ changes/src/lib.rs
@@ -20,3 +20,30 @@ pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
        } 
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_alias() {
+        let mut lights = (0..5)
+            .map(|i| Light::new(&format!("light-{i}")))
+            .collect::<Vec<_>>();
+
+        let alias = "light-3";
+        change_brightness(&mut lights, alias, 100);
+        assert_eq!(lights[3].brightness, 100);
+    }
+
+    #[test]
+    fn test_nonexistent_alias() {
+        let mut lights = (0..5)
+            .map(|i| Light::new(&format!("light-{i}")))
+            .collect::<Vec<_>>();
+
+        let copy = lights.clone();
+        change_brightness(&mut lights, "light-6", 100);
+        assert_eq!(copy, lights);
+    }
+}
