94af782feb7e11295664b8d58f1ebfc8a0d3becb
ilyassatlassi <atlassiilyass@gmail.com>
2025-07-26 00:12:39 +0100

    hashing

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -24,13 +24,15 @@ let  mut sorted_numbers = list.to_vec();
 pub fn mode(list: &[i32]) -> i32 {
     let mut map1 = HashMap::new();
     let mut res = 0;
+    let mut max = 0;
     for num in list {
-        let count = map1.entry(num.clone()).or_insert(0);
+        let count = map1.entry(*num).or_insert(0);
         *count+=1;
-
     }
+
     for (key, value) in map1 {
-        if value > res {
+        if value > max {
+            max = value;
             res = key;
         }
     }
@@ -38,4 +40,33 @@ pub fn mode(list: &[i32]) -> i32 {
     // map1[&res]
     res
 
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::f64;
+
+    #[inline]
+    fn approx_eq(a: f64, b: f64) -> bool {
+        (a - b).abs() < f64::EPSILON
+    }
+
+    #[test]
+    fn test_mean() {
+        let v = [4, 7, 5, 2, 5, 1, 3];
+        assert!(approx_eq(mean(&v), 3.857142857142857));
+    }
+
+    #[test]
+    fn test_median() {
+        assert_eq!(median(&[4, 7, 5, 2, 5, 1, 3]), 4);
+        assert_eq!(median(&[2, 1, 5, 2, 7, 4]), 3);
+        assert_eq!(median(&[1, 7, 5, 5, 6, 4]), 5);
+    }
+
+    #[test]
+    fn test_mode() {
+        let v = [4, 7, 5, 2, 5, 1, 3];
+        assert_eq!(mode(&v), 5);
+    }
 }
