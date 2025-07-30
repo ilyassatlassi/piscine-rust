3b323de7026b0420ad9f52103d9e4eb353ca4cc9
ilyassatlassi <atlassiilyass@gmail.com>
2025-07-31 00:27:21 +0100

    add boxing_todo

--- boxing_todo/src/err.rs
+++ boxing_todo/src/err.rs
@@ -1,46 +1,41 @@
-// use std::{error::Error, fmt::Display};
+use std::{error::Error, fmt::Display};
 
-// #[derive(Debug)]
-// pub enum ParseErr {
-//     // expected public fields
-//     Empty,
-//     Malformed,
-// }
+#[derive(Debug)]
+pub enum ParseErr {
+    // expected public fields
+    Empty,
+    Malformed(Box<dyn Error>),
+}
 
-// impl Display for ParseErr {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         let err = match self {
-//             ParseErr::Empty => "",
-//             ParseErr::Malformed => "",
-//         };
-//         write!(f, "{}", err)
-//     }
-// }
+impl Display for ParseErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", "Failed to parse todo file")
+    }
+}
 
-// impl Error for ParseErr {
-//     fn source(&self) -> Option<&(dyn Error + 'static)> {
-//         Some(self)
-//     }
-// }
+impl Error for ParseErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+           ParseErr::Empty => None, 
+           ParseErr::Malformed(e)=> Some(e.as_ref())
+        }
+    }
+}
 
-// #[derive(Debug)]
-// pub struct ReadErr {
-//     // expected public fields
-//     child_err: Box<dyn Error>,
-// }
+#[derive(Debug)]
+pub struct ReadErr {
+    // expected public fields
+    pub  child_err: Box<dyn Error>,
+}
 
-// impl Display for ReadErr {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         let err = match self.child_err {
-//             ParseErr::Empty => "",
-//             // ParseErr::Malformed => "",
-//         };
-//         write!(f, "{}", err)
-//     }
-// }
+impl Display for ReadErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.child_err)
+    }
+}
 
-// impl Error for ReadErr {
-//     fn source(&self) -> Option<&(dyn Error + 'static)> {
-//         Some(self)
-//     }
-// }
+impl Error for ReadErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(self.child_err.as_ref())
+    }
+}
--- boxing_todo/src/lib.rs
+++ boxing_todo/src/lib.rs
@@ -1,6 +1,7 @@
-// mod err;
+mod err;
+pub use err::{ParseErr, ReadErr};
 use json::parse;
-use std::error::Error;
+use std::{ error::Error};
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Task {
@@ -17,27 +18,123 @@ pub struct TodoList {
 
 impl TodoList {
     pub fn get_todo(path: &str) -> Result<TodoList, Box<dyn Error>> {
-        match parse(path) {
-            Ok(parsed) => {
-                let mut tasks = Vec::new();
+        let file = std::fs::read_to_string(path);
+        match file {
+            Ok(content) => match parse(&content) {
+                Ok(parsed) => {
+                    let mut tasks = Vec::new();
+                    if parsed["tasks"].is_empty() {
+                        return Err(Box::new(err::ParseErr::Empty));
+                    }
+                    for task in parsed["tasks"].members() {
+                        let task = Task {
+                            id: task["id"].as_u32().unwrap(),
+                            description: task["description"].to_string(),
+                            level: task["level"].as_u32().unwrap(),
+                        };
 
-                for task in parsed["tasks"].members() {
-                    let task = Task {
-                        id: task["id"].as_u32().unwrap(),
-                        description: task["description"].to_string(),
-                        level: task["level"].as_u32().unwrap(),
+                        tasks.push(task);
+                    }
+                    let new_todo = TodoList {
+                        title: parsed["title"].to_string(),
+                        tasks: tasks,
                     };
-
-                    tasks.push(task);
+                    Ok(new_todo)
                 }
-                let new_todo = TodoList {
-                    title: parsed["title"].to_string(),
-                    tasks: tasks,
-                };
-                Ok(new_todo)
-            }
-            Err(e) => Err(Box::new(e)),
+                Err(e) => Err(Box::new(err::ParseErr::Malformed(Box::new(e)))),
+            },
+            Err(e) => Err(Box::new(err::ReadErr {
+                child_err: Box::new(e),
+            })),
         }
+
         // todo!()
     }
+}
+
+use json::{object, JsonValue};
+use std::io::{self, Write};
+use tempfile::NamedTempFile;
+
+fn write_and_read_test(
+    write_f: impl FnOnce(&mut NamedTempFile),
+) -> Result<TodoList, Box<dyn Error>> {
+    let mut file = NamedTempFile::new().unwrap();
+    write_f(&mut file);
+
+    let path = file.path().to_str().unwrap();
+    TodoList::get_todo(path)
+}
+
+#[inline]
+fn json_to_file_to_mem(obj: JsonValue) -> Result<TodoList, Box<dyn Error>> {
+    write_and_read_test(|f| obj.write(f).unwrap())
+}
+
+#[test]
+fn test_valid_todo() {
+    let r#struct = TodoList {
+        title: "todo list for something".to_owned(),
+        tasks: vec![
+            Task {
+                id: 0,
+                description: "do this".to_owned(),
+                level: 0,
+            },
+            Task {
+                id: 1,
+                description: "do that".to_owned(),
+                level: 5,
+            },
+        ],
+    };
+
+    let obj = object! {
+        "title" : "todo list for something",
+        "tasks": [
+            { "id": 0, "description": "do this", "level": 0 },
+            { "id": 1, "description": "do that", "level": 5 }
+        ]
+    };
+
+    assert_eq!(r#struct, json_to_file_to_mem(obj).unwrap());
+}
+
+#[test]
+fn test_empty_tasks() {
+    let obj = object! {
+        "title" : "empty tasks",
+        "tasks": []
+    };
+
+    let result = json_to_file_to_mem(obj).unwrap_err();
+
+    assert!(matches!(result.downcast_ref().unwrap(), ParseErr::Empty));
+    assert!(result.source().is_none());
+    assert_eq!(result.to_string(), "Failed to parse todo file");
+}
+
+#[test]
+fn test_read_err() {
+    let result = TodoList::get_todo("invalid_file.json").unwrap_err();
+
+    let ReadErr { child_err } = result.downcast_ref().unwrap();
+
+    assert!(child_err.is::<io::Error>());
+    assert!(result.source().unwrap().is::<io::Error>());
+    assert_eq!(result.to_string(), "Failed to read todo file");
+}
+
+#[test]
+fn test_parse_err_malformed() {
+    let result = write_and_read_test(|f| f.write_all(r#"{"something": ,}"#.as_bytes()).unwrap())
+        .unwrap_err();
+
+    let ParseErr::Malformed(e) = result.downcast_ref::<ParseErr>().unwrap() else {
+        panic!()
+    };
+
+    assert!(e.is::<json::Error>());
+    assert!(result.source().unwrap().is::<ParseErr>());
+    assert_eq!(result.to_string(), "Failed to parse todo file");
 }
