663cc45cc5963db798566c69a8dcf5328b18f976
ilyassatlassi <ilyassatlassi@gmail.com>
2025-07-30 13:10:31 +0100

    add error_types

---
+++ error_types/src/lib.rs
@@ -0,0 +1,135 @@
+use chrono::{Local, Utc};
+// this will be the structure that wil handle the errors
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    // expected public fields
+    form_values: ( &'static str, String),
+    date: String,
+    err: &'static str,
+}
+
+impl FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        Self {
+            form_values: (field_name, field_value),
+            date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            err: err,
+        }
+        // todo!()
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    // expected public fields
+    pub name: String,
+    pub password: String,
+}
+
+impl Form {
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name.is_empty() {
+            return Err(FormError::new(
+                "name",
+                self.name.to_string(),
+                "Username is empty",
+            ));
+        }
+        if self.password.chars().count() < 8 {
+            return Err(FormError::new(
+                "password",
+                self.password.to_string(),
+                "Password should be at least 8 characters long",
+            ));
+        }
+        if !(self.password.chars().any(|char| char.is_ascii_alphabetic())
+            && self
+                .password
+                .chars()
+                .any(|char| char.is_ascii_punctuation())
+            && self
+                .password
+                .chars()
+                .any(|char| char.is_ascii_digit()))
+        {
+            return Err(FormError::new(
+                "password",
+                self.password.to_string(),
+                "Password should be a combination of ASCII numbers, letters and symbols",
+            ));
+        }
+        Ok(())
+        // todo!()
+    }
+}
+#[test]
+fn test_error_type() {
+    let cases = [
+        (
+            Form {
+                name: "Katy".to_owned(),
+                password: "qwTw12&%$3sa1dty_".to_owned(),
+            },
+            Ok(()),
+        ),
+        (
+            Form {
+                name: "".to_owned(),
+                password: "qwTw12&%$3sa1dty_".to_owned(),
+            },
+            Err(FormError {
+                form_values: ("name", "".to_owned()),
+                date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                err: "Username is empty",
+            }),
+        ),
+        (
+            Form {
+                name: "Someone".to_owned(),
+                password: "12345".to_owned(),
+            },
+            Err(FormError {
+                form_values: ("password", "12345".to_owned()),
+                date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                err: "Password should be at least 8 characters long",
+            }),
+        ),
+        (
+            Form {
+                name: "Someone".to_owned(),
+                password: "sdASDsrW".to_owned(),
+            },
+            Err(FormError {
+                form_values: ("password", "sdASDsrW".to_owned()),
+                date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                err: "Password should be a combination of ASCII numbers, letters and symbols",
+            }),
+        ),
+        (
+            Form {
+                name: "Someone".to_owned(),
+                password: "dsGE1SAD213".to_owned(),
+            },
+            Err(FormError {
+                form_values: ("password", "dsGE1SAD213".to_owned()),
+                date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                err: "Password should be a combination of ASCII numbers, letters and symbols",
+            }),
+        ),
+        (
+            Form {
+                name: "Someone".to_owned(),
+                password: "dsaSD&%DF!?=".to_owned(),
+            },
+            Err(FormError {
+                form_values: ("password", String::from("dsaSD&%DF!?=")),
+                date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                err: "Password should be a combination of ASCII numbers, letters and symbols",
+            }),
+        ),
+    ];
+
+    for (form, expected) in cases {
+        assert_eq!(form.validate(), expected);
+    }
+}
