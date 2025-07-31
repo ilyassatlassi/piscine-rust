48b72a50835a86bf2501cb8bfdae08a7b9b3e55a
ilyassatlassi <atlassiilyass@gmail.com>
2025-07-31 20:30:24 +0100

    fix handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,17 +1,16 @@
 use std::fs::File;
 use std::io::{ErrorKind, Write};
 
+use std::fs::OpenOptions;
 use std::path::Path;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    let mut greeting_file = File::open(path).unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create(path).unwrap_or_else(|error| {
-                panic!("Problem creating the file: {error:?}");
-            })
-        } else {
-            panic!("Problem opening the file: {error:?}");
-        }
-    });
-    greeting_file.write(content.as_bytes()).unwrap();
+    let mut file = OpenOptions::new()
+        .append(true)
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)
+        .unwrap();
+    file.write(content.as_bytes()).unwrap();
 }
--- handling/src/main.rs
+++ handling/src/main.rs
@@ -1,3 +1,4 @@
+use handling::*;
 use std::fs;
 
 fn main() {
@@ -7,4 +8,57 @@ fn main() {
 
     let contents = fs::read_to_string(path).unwrap();
     println!("{}", contents);
+}
+use std::io::Write;
+use tempfile::NamedTempFile;
+
+#[test]
+fn test_with_non_existing_file() {
+    let file = NamedTempFile::new().unwrap();
+    let path = file.path().to_path_buf();
+    file.close().unwrap();
+    let content = "hello world!";
+
+    handling::open_or_create(&path, content);
+
+    assert_eq!(content, fs::read_to_string(path).unwrap());
+}
+
+#[test]
+fn test_with_empty_file() {
+    let path = NamedTempFile::new().unwrap().into_temp_path();
+    let content = "hello world!";
+
+    handling::open_or_create(&path, content);
+
+    assert_eq!(content, fs::read_to_string(path).unwrap());
+}
+
+#[test]
+fn test_with_file() {
+    let mut file = NamedTempFile::new().unwrap();
+    let initial_content = "some content\n";
+    file.write_all(initial_content.as_bytes()).unwrap();
+    let path = file.into_temp_path();
+    let content = "hello world!";
+
+    handling::open_or_create(&path, content);
+
+    assert_eq!(
+        format!("{}{}", initial_content, content),
+        fs::read_to_string(path).unwrap()
+    );
+}
+
+#[test]
+#[should_panic]
+fn test_with_file_with_insufficient_permissions() {
+    let file = NamedTempFile::new().unwrap();
+    let mut permissions = file.as_file().metadata().unwrap().permissions();
+    permissions.set_readonly(true);
+    file.as_file().set_permissions(permissions).unwrap();
+    let path = file.into_temp_path();
+    let content = "hello world!";
+
+    handling::open_or_create(&path, content);
 }
