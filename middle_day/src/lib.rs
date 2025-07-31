6c8219a50737aa197eba33b1293ad46c04b461d3
ilyassatlassi <atlassiilyass@gmail.com>
2025-07-31 21:15:04 +0100

    add middle_day

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,13 +1,35 @@
 // use chrono::*;
 
+use chrono::Datelike;
+
 pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
     if is_leap_year(year) {
-       return None ; 
+        return None;
     }
-   let middle = chrono::NaiveDate::from_yo_opt(year as i32, 183);
-   Some()
+    let middle = chrono::NaiveDate::from_yo_opt(year as i32, 183)?;
+       Some(middle.weekday())
+    // println!("{:?}", middle.day());
     // todo!()
 }
 fn is_leap_year(year: u32) -> bool {
     (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+}
+use chrono::Weekday;
+// use middle_day::*;
+
+#[test]
+fn leap_years() {
+    assert!(middle_day(1892).is_none(), "1892 was a leap year!");
+    assert!(middle_day(1904).is_none(), "1904 was a leap year!");
+    assert!(middle_day(2012).is_none(), "2012 was a leap year!");
+}
+
+#[test]
+fn weekdays() {
+    assert_eq!(Weekday::Tue, middle_day(2019).unwrap());
+    assert_eq!(Weekday::Wed, middle_day(1997).unwrap());
+    assert_eq!(Weekday::Mon, middle_day(1663).unwrap());
+    assert_eq!(Weekday::Wed, middle_day(1873).unwrap());
+    assert_eq!(Weekday::Thu, middle_day(1953).unwrap());
+    assert_eq!(Weekday::Wed, middle_day(1879).unwrap());
 }
--- profanity-filter/src/main.rs
+++ profanity-filter/src/main.rs
@@ -23,3 +23,18 @@ fn test_ok_ms() {
     .into_iter()
     .for_each(|m| assert_eq!(Ok(m), profanity_filter::check_ms(m)));
 }
+// use profanity_filter::*;
+
+// fn main() {
+//     let m0 = Message::new("hello there".to_string(), "toby".to_string());
+//     println!("{:?}", check_ms(&m0));
+
+//     let m1 = Message::new("".to_string(), "toby".to_string());
+//     println!("{:?}", check_ms(&m1));
+
+//     let m2 = Message::new("you are stupid".to_string(), "toby".to_string());
+//     println!("{:?}", check_ms(&m2));
+
+//     let m3 = Message::new("stupid".to_string(), "toby".to_string());
+//     println!("{:?}", check_ms(&m3));
+// }
