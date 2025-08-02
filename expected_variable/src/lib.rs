9ab403381e81da03ca176687b67b9c4640c615c6
ilyassatlassi <ilyassatlassi@gmail.com>
2025-08-03 00:13:15 +0100

    add some

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,37 +1,27 @@
 use convert_case::{Case, Casing};
 use edit_distance::edit_distance;
-// pub fn expected_variable(compare: &str, expected: &str) -> Option<String> {
-//     // Some("".to_string())
-//     let compare = compare.to_lowercase();
-//     let expected = expected.to_lowercase();
-//     if !compare.is_case(Case::Camel) && !compare.to_lowercase().is_case(Case::Snake) {
-//         println!("compare is not camel nor snake {}", compare);
-//         return None;
-//     }
-    
-//     let dis = edit_distance(&compare, &expected);
-//     let per = (100. - (dis as f64 / expected.len() as f64) * 100.).round();
-//     // println!("{}", dis);
-//     if per >= 50. {
-//         Some(format!("{per}%"))
-//     } else {
-//         None
-//     }
-// }
 pub fn expected_variable(compare: &str, expected: &str) -> Option<String> {
-    let compare = compare.to_lowercase();
-    let expected = expected.to_lowercase();
 
-    if !compare.is_case(Case::Camel) && !compare.is_case(Case::Snake) {
+    if !compare.chars().any(|c| c == '_' || c.is_uppercase()) {
+        return None;
+    } 
+
+    // Some("".to_string())
+    //let compare = compare.to_lowercase();
+    let expected = expected.to_lowercase();
+    if !compare.is_case(Case::Camel) && !compare.to_lowercase().is_case(Case::Snake) {
+        println!("compare is not camel nor snake {}", compare);
         return None;
     }
-
-    let distance = edit_distance(&compare, &expected);
-
-    let percentage = 100 - (distance * 100 / expected.len()).min(100);
-    if percentage >= 50 {
-        Some(format!("{percentage}%"))
+    
+    let dis = edit_distance(&compare, &expected);
+    let per = (100. - (dis as f64 / expected.len() as f64) * 100.).round();
+    // println!("{}", dis);
+    if per >= 50. {
+        Some(format!("{per}%"))
     } else {
         None
     }
 }
+
+
