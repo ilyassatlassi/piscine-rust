e627c4cbc845ebd6038759e755fee611e9ee81c1
ilyassatlassi <atlassiilyass@gmail.com>
2025-08-04 01:47:12 +0100

    add dress_code

--- dress_code/src/lib.rs
+++ dress_code/src/lib.rs
@@ -3,4 +3,47 @@ pub struct Outfit {
     pub jacket: Jacket,
     pub hat: Hat,
 }
-pub fn choose_outfit(formality_level: Option<u32>, invitation_message: Result<&str, &str>) -> Outfit {}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Jacket {
+    Black,
+    White,
+    Flowers,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Hat {
+    Snapback,
+    Baseball,
+    Fedora,
+}
+
+pub fn choose_outfit(
+    formality_level: Option<u32>,
+    invitation_message: Result<&str, &str>,
+) -> Outfit {
+    let mut out = Outfit {
+        jacket: Jacket::Black,
+        hat: Hat::Baseball,
+    };
+    match formality_level {
+        Some(num) => {
+            if num > 0 {
+                out.jacket = Jacket::White;
+            } else {
+                out.jacket = Jacket::Black
+            }
+        }
+        None => out.jacket = Jacket::Flowers,
+    }
+    match invitation_message {
+        Ok(_) => out.hat = Hat::Fedora,
+        Err(_) => {
+            out.hat = Hat::Snapback;
+            if    out.jacket == Jacket::Flowers {
+                out.hat = Hat::Baseball
+            }
+        }
+    }
+    out
+}
